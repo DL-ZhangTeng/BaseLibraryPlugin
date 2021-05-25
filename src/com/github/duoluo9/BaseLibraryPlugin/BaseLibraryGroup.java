c68bcaa9c235e3836feac42638b9e54981449fbb
张腾 <763263311@qq.com>
2021-05-25 10:00:48 +0800

    按钮位置调整

---
+++ src/com/github/duoluo9/BaseLibraryPlugin/BaseLibraryGroup.java
@@ -0,0 +1,13 @@
+package com.github.duoluo9.BaseLibraryPlugin;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import org.jetbrains.annotations.NotNull;
+
+public class BaseLibraryGroup extends DefaultActionGroup {
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        super.update(e);
+    }
+}
